059f567987bba8fbc7824884b6c03ed247d79ad6
cimchun <cimchun.me@gmail.com>
2024-12-18 13:13:52 +0700

    's

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 // src/App.js
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import FolderList from "./pages/ViewListFolder/ViewListFolder.jsx";
 import EditQuyHoach from "./pages/EditQuyHoach/EditQuyHoach.jsx";
+import ManagerAccount from "./pages/managerAccount/ManagerAccount.jsx";
+import VipUpgrade from "./pages/VipUpgrade/VipUpgrade.jsx";
 
 function App() {
   return (
@@ -10,6 +17,8 @@ function App() {
         <Route path="/" element={<Navigate to="/editquyhoach" />} />
         <Route path="/listfolders" element={<FolderList />} />
         <Route path="/editquyhoach" element={<EditQuyHoach />} />
+        <Route path="/manageraccount" element=<ManagerAccount /> />
+        <Route path="/vipupgrade" element={<VipUpgrade />} />
       </Routes>
     </Router>
   );
