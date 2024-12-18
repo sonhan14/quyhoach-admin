e0cadcfa0e9ec0a23d8debcd69d45439b7b27597
sonhan14 <sonhan14@gmail.com>
2024-12-18 17:38:52 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // src/App.js
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FolderList from "./pages/ViewListFolder/ViewListFolder.jsx";
 import EditQuyHoach from "./pages/EditQuyHoach/EditQuyHoach.jsx";
+import ManagerAccount from "./pages/managerAccount/ManagerAccount.jsx";
+import VipUpgrade from "./pages/VipUpgrade/VipUpgrade.jsx";
+
 import ImageListFolder from "./pages/ViewListImage/ViewListImage.jsx";
 import Home from "./pages/Home/index.jsx";
 import ViewImageInFolder from "./pages/ViewImageInFolder/ViewImageInFolder.jsx";
-
+import LevelListFolder from "./pages/ViewListLevel/ViewListLevel.jsx";
+import IDListFolder from "./pages/ViewListId/ViewListId.jsx";
 function App() {
   return (
     <Router>
@@ -17,8 +17,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/listfolders" element={<FolderList />} />
         <Route path="/editquyhoach" element={<EditQuyHoach />} />
+        <Route path="/manageraccount" element={<ManagerAccount />} />
+        <Route path="/vipupgrade" element={<VipUpgrade />} />
         <Route path="/imagefolderlist" element={<ImageListFolder />} />
-        <Route path="/viewimageinfolder/:city/:level/:id" element={<ViewImageInFolder />} />
+        <Route path="/imagefolderlist/:city" element={<LevelListFolder />} />
+        <Route path="/imagefolderlist/:city/:level" element={<IDListFolder />} />
+        <Route path="/imagefolderlist/:city/:level/:id" element={<ViewImageInFolder />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/managerAccount/ManagerAccount.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import "./ManagerAccount.css";
+import { LuRefreshCw } from "react-icons/lu";
+import { TbExclamationMark } from "react-icons/tb";
+import Search from "../../components/Search/Search.jsx";
+
+const ManagerAccount = () => {
+    const [isOnline, setIsOnline] = useState(true);
+
+    return (
+        <div className="container">
+            <div className="col-8">
+                <div className="search-container">
+                    <Search />
+                    <button className="refresh-btn">
+                        <LuRefreshCw />
+                    </button>
+                </div>
+
+                <div className="user-info">
+                    <div className="user-avatar">
+                        <img
+                            src="https://www.w3schools.com/howto/img_avatar.png"
+                            alt="User Avatar"
+                            className="avatar"
+                        />
+                        <div className={`status ${isOnline ? "online" : "offline"}`} />
+                    </div>
+                    <div className="user-details">
+                        <h4>User name</h4>
+                        <p>Abcd1423@mail.com</p>
+                    </div>
+                    <div className="notifications">
+                        <button className="notification-btn">
+                            <TbExclamationMark />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-4">
+                <div>
+                    <div className="new-register">Đăng ký mới</div>
+                    <div>
+                        <input type="checkbox" />
+                        <label>Tuần</label>
+                        <input type="checkbox" />
+                        <label>Ngày</label>
+                        <input type="checkbox" />
+                        <label>Giờ</label>
+                    </div>
+                </div>
+                <div className="user-info">
+                    <div className="user-avatar">
+                        <img
+                            src="https://www.w3schools.com/howto/img_avatar.png"
+                            alt="User Avatar"
+                            className="avatar"
+                        />
+                        <div className={`status ${isOnline ? "online" : "offline"}`} />
+                    </div>
+                    <div className="user-details">
+                        <h4>User name</h4>
+                        <p>Abcd1423@mail.com</p>
+                        <i>3/12/2024</i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ManagerAccount;
